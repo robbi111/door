f06421732249cedfaa0f5347ccc84e10883f99a8
Robert <robbi111@users.noreply.github.com>
2018-06-25 22:27:12 +0200

    add main.c

--- software/Door/Door/main.c
+++ software/Door/Door/main.c
@@ -1,10 +1,11 @@
 #include <atmel_start.h>
+#include <globals.h>
 
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
-
+	
 	/* Replace with your application code */
 	while (1) {
 	}
